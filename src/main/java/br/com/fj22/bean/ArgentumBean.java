b7bde2d3e4114a15d2d5a39c78cc3a6ca406334e
Leonardo Narciso <lnarcisoov@gmail.com>
2017-05-24 16:03:16 -0300

    [JSF] Adicionando viewscoped no bean e ordenacao por preço na tela de negociacoes..

--- src/main/java/br/com/fj22/bean/ArgentumBean.java
+++ src/main/java/br/com/fj22/bean/ArgentumBean.java
@@ -1,15 +1,20 @@
 package br.com.fj22.bean;
 
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.fj22.modelo.Negociacao;
 import br.com.fj22.servico.ClienteWebService;
 
 @ManagedBean
-public class ArgentumBean {
+@ViewScoped
+public class ArgentumBean implements Serializable{
+	
+	private static final long serialVersionUID = -8012961205285780771L;
 	
 	private List<Negociacao> negociacoes;
 	
